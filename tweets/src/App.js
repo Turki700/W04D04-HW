40066bc58c76baee9302c7b5900cd857a802f0ed
abrarZhr <Abrar.alzhrani.ruh.js@tuwaiq.edu.sa>
2021-10-25 18:26:38 +0300

    abrar Done

---
+++ tweets/src/App.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+
+const UserInfo =[
+{
+  userName :"Max" ,
+  imag:"",
+
+  Tweets :[
+  "Be a game-change. The world is already full of players"
+]
+
+},
+
+{
+  userName :"ماجد" ,
+  imag:"",
+
+  Tweets :[
+  "l have not failed. l've just found 10,000 ways that won't work - Thomas A.Edison"
+]
+
+},
+
+{
+
+  userName :"joudy " ,
+  imag:"",
+
+  Tweets :[
+  "if you look at what you have in life , you'll always have more. if you look at what you don't have in life , you'll never have enough-Oprah winfrey " , 
+  " you will face many defeats in life , but never let yourself be defeated -Maya Angelou"
+]
+
+}
+
+
+]
+
+
+function App() {
+  return (
+    <div className="App">
+      htgfhfg
+    </div>
+
+
+  );
+}
+
+export default App;
